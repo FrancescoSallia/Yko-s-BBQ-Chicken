71f813ebb3cd4f80c5aa44ab86df0119fd6124b2
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-10-01 21:16:30 +0200

    WIP timer

--- lib/Pages/timer_page.dart
+++ lib/Pages/timer_page.dart
@@ -47,6 +47,7 @@ class TimerPage extends StatelessWidget {
                       ),
                     ),
                   ),
+
                   //Status Text
                   Positioned(
                     bottom: 0,
@@ -137,6 +138,128 @@ class TimerPage extends StatelessWidget {
                       child: Lottie.asset(
                         animate: true,
                         'assets/animations/prepare_food.json',
+                        repeat: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 10), // Abstand zu den Rows
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 8,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  //das Rechteck vom Timer
+                  Container(
+                    height: 180,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black.withValues(alpha: 0.4),
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(600),
+                      ),
+                    ),
+                  ),
+
+                  //Status Text
+                  Positioned(
+                    bottom: 0,
+                    right: 10,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 150,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Text(
+                              "Geliefert",
+                              style: GoogleFonts.inter(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 20,
+                    child: SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: Lottie.asset(
+                        animate: true,
+                        'assets/animations/delivered.json',
+                        repeat: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              //Right Corner
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  //das Rechteck vom Timer
+                  Container(
+                    height: 180,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black.withValues(alpha: 0.4),
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(600),
+                      ),
+                    ),
+                  ),
+                  //Status Text
+                  Positioned(
+                    bottom: 0,
+                    right: 10,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 150,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Text(
+                              "Auf dem Weg",
+                              style: GoogleFonts.inter(
+                                // fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    child: SizedBox(
+                      width: 110,
+                      height: 110,
+                      child: Lottie.asset(
+                        animate: true,
+                        'assets/animations/delivery_riding.json',
                         repeat: true,
                       ),
                     ),
